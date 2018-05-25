f16173059053c17b3327e795b957286a3aae0082
bhawneshdipu <bhawnesh.dipu@gmail.com>
2018-05-26 02:44:16 +0530

    Admin

--- src/main/java/com/manish/dao/CourseConductionDao.java
+++ src/main/java/com/manish/dao/CourseConductionDao.java
@@ -1,14 +1,164 @@
 package com.manish.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.manish.model.CourseConduction;
+
 public class CourseConductionDao {
 
-	@Autowired
-	DataSource mysqlDataSource;
-	
-	private String tableName="lecturer";
+		@Autowired
+		DataSource mysqlDataSource;
+		
+		private String tableName="course_conduction";
+		public final String insertValueList=" LID , CID, SEMESTER , CAPACITY  ";
+		public final String valueOrderList=" CCID, LID , CID, SEMESTER , CAPACITY ";
+		
+		public final String values=" ?, ?, ? , ? ";
+		public final String updateValueList=" LID=? , CID =? , SEMESTER=? , CAPACITY=? ";
+		
+		public final String courseConductionInsertPstmt="INSERT INTO "+tableName+" ( "+insertValueList+")"+ "  VALUES ( "+values+" ) ";
+		
+		public final String courseConductionUpdatePstmt="UPDATE "+tableName+" SET "+updateValueList +" WHERE CAID=? ";
+		
+		public final String courseConductionFindPstmt="SELECT * FROM "+tableName+" WHERE CCID=? ";
+		
+		public final String courseConductionDeletePstmt="DELETE  FROM "+tableName+" WHERE CCID=? ";
+		
+		public final String courseConductionAllPstmt="SELECT * FROM "+tableName+" ";
+		
+		
+		public int insert(CourseConduction obj) {
+			try {
+				Connection conn=mysqlDataSource.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement(courseConductionInsertPstmt);
+				pstmt.setInt(1, obj.getLid());
+				pstmt.setInt(2, obj.getCid());
+				pstmt.setString(3, obj.getSemester());
+				pstmt.setInt(4, obj.getCapacity());
+				
+				
+				int countInsert=pstmt.executeUpdate();
+				
+				if(countInsert>0){
+						System.out.println("CourseConduction Insert Success");
+					return countInsert;
+				}else{
+					return 0;
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return -1;
+			}
+			
+		
+		}
+		public int update(CourseConduction obj) {
+			try {
+				Connection conn=mysqlDataSource.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement(courseConductionUpdatePstmt);
+				pstmt.setInt(1, obj.getLid());
+				pstmt.setInt(2, obj.getCid());
+				pstmt.setString(3, obj.getSemester());
+				pstmt.setInt(4, obj.getCapacity());
+				pstmt.setInt(5, obj.getCcid());
+				
+				int countUpdate=pstmt.executeUpdate();
+				
+				if(countUpdate>0){
+						System.out.println("CourseConduction Update Success");
+					return countUpdate;
+				}else{
+					return 0;
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return -1;
+			}
+			
+		
+		}
+		public CourseConduction[] all(){
+			try {
+				Connection conn=mysqlDataSource.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement(courseConductionAllPstmt);
+				ResultSet rs=pstmt.executeQuery();
+				List<CourseConduction> arr=new ArrayList<CourseConduction>();
+				while(rs.next()){
+					CourseConduction obj=new CourseConduction();
+					
+					obj.setLid(rs.getInt("LID"));
+					obj.setCid(rs.getInt("CID"));
+					
+					obj.setCcid(rs.getInt("CCID"));
+					obj.setSemester(rs.getString("SEMESTER"));
+
+					obj.setCapacity(rs.getInt("CAPACITY"));
+					arr.add(obj);
+				}
+				rs.close();
+				return arr.toArray(new CourseConduction[arr.size()]);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+			
+			
+		}
+		public CourseConduction find(int id){
+			try {
+				Connection conn=mysqlDataSource.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement(courseConductionFindPstmt);
+				pstmt.setInt(1, id);
+				ResultSet rs=pstmt.executeQuery();
+				
+				CourseConduction obj=new CourseConduction();
+				
+				while(rs.next()){
+					obj.setLid(rs.getInt("LID"));
+					obj.setCid(rs.getInt("CID"));
+					
+					obj.setCcid(rs.getInt("CCID"));
+					obj.setSemester(rs.getString("SEMESTER"));
+
+					obj.setCapacity(rs.getInt("CAPACITY"));
+					
+					break;
+				}
+				rs.close();
+				return obj;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+			
+			
+		}
+		public int delete(int id){
+			try {
+				Connection conn=mysqlDataSource.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement(courseConductionDeletePstmt);
+				pstmt.setInt(1, id);
+				int deleteCount=pstmt.executeUpdate();
+				return deleteCount;
+			}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return -1;
+			}
+		}
+		
 	
 }
---
+++ src/main/java/com/manish/enterprise/CourseConductionController.java
@@ -0,0 +1,178 @@
+package com.manish.enterprise;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.gson.Gson;
+import com.manish.dao.AssessmentDao;
+import com.manish.dao.CourseConductionDao;
+import com.manish.dao.CourseConductionDao;
+import com.manish.dao.CourseDao;
+import com.manish.dao.LecturerDao;
+import com.manish.dao.StudentDao;
+import com.manish.model.Assessment;
+import com.manish.model.Course;
+import com.manish.model.CourseConduction;
+import com.manish.model.Lecturer;
+import com.manish.model.Student;
+
+@Controller
+public class CourseConductionController{
+	
+	@Autowired
+	DataSource mysqlDataSource;
+	@Autowired
+	Gson gson;
+	
+	@Autowired
+	LecturerDao lecturerDao;
+	
+	@Autowired
+	CourseDao courseDao;
+	
+	@Autowired
+	AssessmentDao assessmentDao;
+	
+	@Autowired
+	CourseConductionDao courseAssessmentDao;
+	
+	@Autowired
+	CourseConductionDao courseConductionDao;
+	
+	@Autowired
+	StudentDao studentDao;
+
+	/*----------------------------------------------------------------------------------------------*/
+
+	@RequestMapping("/admin/course_conductions")	
+	String courseConductions(HttpServletRequest request) {
+		CourseConduction[] courseConductionList=courseConductionDao.all();
+		System.out.println("CourseConduction List:\n:"+gson.toJson(courseConductionList));
+		request.getSession().setAttribute("courseConductionList", courseConductionList);
+		
+		return "course_conductions";
+	}
+	@RequestMapping(value="/admin/course_conduction/add",method=RequestMethod.GET)	
+	String courseConduction_add(HttpServletRequest request) {
+		Course[] courseList=courseDao.all();
+		System.out.println("Course List:\n:"+gson.toJson(courseList));
+		request.getSession().setAttribute("courseList", courseList);
+		Lecturer[] lecturerList=lecturerDao.all();
+		System.out.println("lecturer  List:\n:"+gson.toJson(lecturerList));
+		request.getSession().setAttribute("lecturerList", lecturerList);
+
+		return "course_conduction_add";
+	}
+	@RequestMapping(value="/admin/course_conduction/add",method=RequestMethod.POST)	
+	public void courseConduction_store(HttpServletRequest request,HttpServletResponse response) {
+		CourseConduction obj=new CourseConduction();
+		obj.setLid(Integer.parseInt(request.getParameter("lid")));
+		obj.setCid(Integer.parseInt(request.getParameter("cid").toString()));
+		obj.setSemester(request.getParameter("semester").toString());
+		obj.setCapacity(Integer.parseInt(request.getParameter("capacity").toString()));
+		
+		int result=courseConductionDao.insert(obj);
+		 try {
+			response.sendRedirect("/admin/course_conductions");
+			return;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 return;
+	}
+	@RequestMapping(value="/admin/course_conduction/view/{id}",method=RequestMethod.GET)	
+	String courseConduction_view(@PathVariable("id") int id,HttpServletRequest request) {
+		CourseConduction course_conduction=courseConductionDao.find(id);
+	
+		Course[] courseList=courseDao.all();
+		System.out.println("Course List:\n:"+gson.toJson(courseList));
+		request.getSession().setAttribute("courseList", courseList);
+		Lecturer[] lecturerList=lecturerDao.all();
+		System.out.println("lecturer  List:\n:"+gson.toJson(lecturerList));
+		request.getSession().setAttribute("lecturerList", lecturerList);
+		request.getSession().setAttribute("course_conduction", course_conduction);
+
+		return "course_conduction_view";
+	}
+	@RequestMapping(value="/admin/course_conduction/edit/{id}",method=RequestMethod.GET)	
+	String courseConduction_edit(@PathVariable("id") int id,HttpServletRequest request) {
+		CourseConduction course_conduction=courseConductionDao.find(id);
+		
+		Course[] courseList=courseDao.all();
+		System.out.println("Course List:\n:"+gson.toJson(courseList));
+		request.getSession().setAttribute("courseList", courseList);
+		Lecturer[] lecturerList=lecturerDao.all();
+		System.out.println("lecturer  List:\n:"+gson.toJson(lecturerList));
+		request.getSession().setAttribute("lecturerList", lecturerList);
+		request.getSession().setAttribute("course_conduction", course_conduction);
+		
+		
+		return "course_conduction_edit";
+	}
+	@RequestMapping(value="/admin/course_conduction/edit/{id}",method=RequestMethod.POST)	
+	public void courseConduction_update(@PathVariable("id") int id,HttpServletRequest request,HttpServletResponse response) {
+		CourseConduction obj=new CourseConduction();
+		obj.setCcid(id);
+		obj.setLid(Integer.parseInt(request.getParameter("lid")));
+		obj.setCid(Integer.parseInt(request.getParameter("cid").toString()));
+		obj.setSemester(request.getParameter("semester").toString());
+		obj.setCapacity(Integer.parseInt(request.getParameter("capacity").toString()));
+		int result=courseConductionDao.update(obj);
+		 try {
+			response.sendRedirect("/admin/course_conductions");
+			return;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 return;
+	}
+	@RequestMapping(value="/admin/course_conduction/delete/{id}",method=RequestMethod.GET)	
+	String courseConduction_delete(@PathVariable("id") int id,HttpServletRequest request) {
+		CourseConduction course_conduction=courseConductionDao.find(id);
+		
+		Course[] courseList=courseDao.all();
+		System.out.println("Course List:\n:"+gson.toJson(courseList));
+		request.getSession().setAttribute("courseList", courseList);
+		Lecturer[] lecturerList=lecturerDao.all();
+		System.out.println("lecturer  List:\n:"+gson.toJson(lecturerList));
+		request.getSession().setAttribute("lecturerList", lecturerList);
+
+		request.getSession().setAttribute("course_conduction", course_conduction);
+		return "course_conduction_delete";
+	}
+
+	
+	@RequestMapping(value="/admin/course_conduction/delete/{id}",method=RequestMethod.POST)	
+	public void courseConduction_destroy(@PathVariable("id") int id,HttpServletRequest request,HttpServletResponse response) {
+
+		int deleteCount=courseConductionDao.delete(id);
+		try {
+			response.sendRedirect("/admin/course_conductions");
+			return;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 return;
+	}
+	
+	
+	
+	
+	
+	
+	/*---------------------------------------------------------------------------------------------------*/
+	
+
+}
